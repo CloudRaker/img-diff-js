b4c06c684a9b59bd9426643c929f287303f79f39
Antoine Beauvais-Lacasse <alacasse@cloudraker.com>
2020-02-04 15:28:26 -0500

    Fix type declarations

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,39 +1,57 @@
 /// <reference types="node" />
 
-export interface ImgDiffOptions {
+declare module '@cloudraker/img-diff-js' {
 
-  actualFilename?: string;
-  actualReadStream?: NodeJS.ReadableStream;
-  actualType?: string;
+  export interface ImgDiffOptions {
 
-  expectedFilename?: string;
-  expectedReadStream?: NodeJS.ReadableStream;
-  expectedType?: string;
+    actualFilename?: string;
+    actualReadStream?: NodeJS.ReadableStream;
+    actualData?: Buffer;
+    actualType?: keyof Decoders;
 
-  diffWriteStream?: NodeJS.WritableStream;
-  diffFilename?: string;
+    expectedFilename?: string;
+    expectedReadStream?: NodeJS.ReadableStream;
+    expectedData?: Buffer;
+    expectedType?: keyof Decoders;
+
+    diffWriteStream?: NodeJS.WritableStream;
+    diffFilename?: string;
+    generateOnlyDiffFile?: boolean;
+
+  }
+
+  export interface ImgDiffResult {
+    width: number;
+    height: number;
+    imagesAreSame: boolean;
+    diffCount: number;
+  }
+
+  export function imgDiff(opt: ImgDiffOptions): Promise<ImgDiffResult>;
+
+  export interface ImgDiffDecodeResult {
+    width: number;
+    height: number;
+    data: Uint8Array;
+  }
+
+  export interface ImgDiffDecoder {
+    decodeBuffer(buffer: Buffer): Promise<ImgDiffDecodeResult>;
+
+    decodeStream(stream: NodeJS.ReadableStream): Promise<ImgDiffDecodeResult>;
+
+    decodeFile(filename: string): Promise<ImgDiffDecodeResult>;
+  }
+
+  /**
+   * Augment this interface & use `registerDecoder` to accept more formats.
+   */
+  export interface Decoders {
+    'jpg': ImgDiffDecoder;
+    'jpeg': ImgDiffDecoder;
+    'png': ImgDiffDecoder;
+  }
+
+  export function registerDecoder(extensions: string[], decoder: ImgDiffDecoder);
 
 }
-
-export interface ImgDiffResult {
-  width: number;
-  height: number;
-  imagesAreSame: boolean;
-  diffCount: number;
-}
-
-export function imgDiff(opt: ImgDiffOptions): ImgDiffResult;
-
-export interface ImgDiffDecodeResult {
-  width: number;
-  height: number;
-  data: Uint8Array;
-}
-
-export interface ImgDiffDecoder {
-  decodeFile(filename: string): ImgDiffDecodeResult;
-
-  decodeStream(stream: NodeJS.ReadableStream): ImgDiffDecodeResult;
-}
-
-export function registerDecoder(extensions: string[], decoder: ImgDiffDecoder);
